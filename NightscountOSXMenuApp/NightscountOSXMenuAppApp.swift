79d0acc403922f83ba50c16aea9fe2f81fea5624
Adam Dinneen <adam.d@walkme.com>
2022-06-27 21:33:29 +1000

    Initial Commit

---
+++ NightscountOSXMenuApp/NightscountOSXMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightscountOSXMenuAppApp.swift
+//  NightscountOSXMenuApp
+//
+//  Created by adam.d on 27/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct NightscountOSXMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
